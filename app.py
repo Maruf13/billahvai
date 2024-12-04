d483d7d9523af2ab70592f4a6eb71d74ffe3fff0
Mohammad Mahmudul Islam <marufrusafi13.cuet@gmail.com>
2024-12-05 05:19:06 +0600

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ from typing import List
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 
-port = int(os.environ.get("PORT", 8000))
-uvicorn.run(app, host="0.0.0.0", port=port)
-
 app = FastAPI()
 
 # Mount the frontend directory
